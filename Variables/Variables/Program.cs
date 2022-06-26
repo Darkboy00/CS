4aa033fd7855f4d3fd5809ee0493891909468230
MehmetKaymakçı <61612352+Darkboy00@users.noreply.github.com>
2022-06-26 12:10:32 +0300

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string city, district;
+            //city = "Istanbul";
+            //district = "Esenler";
+            //Console.Write(city + "-" + district);
+            //Console.Read();
+
+            //int num = 24;
+            //Console.Write(num);
+            //Console.Read();
+
+            //int num1, num2, total;
+            //num1 = 20;
+            //num2 = 15;
+            //total = num1 + num2;
+            //Console.Write(total);
+            //Console.Read();
+
+            //int shortEdge, longEdge, area, perimeter;
+            //shortEdge = 10;
+            //longEdge = 20;
+            //area = shortEdge * longEdge;
+            //perimeter = 2 * area;
+            //Console.Write("area:" + area + " " + "perimeter:" + perimeter);
+            //Console.Read();
+
+            //int n1, n2, total, multiplication, divide, difference;
+            //n1 = 20;
+            //n2 = 5;
+            //total = n1 + n2;
+            //multiplication = n1 * n2;
+            //divide = n1 / n2;
+            //difference = n1 - n2;
+            //Console.WriteLine("total:" + total);
+            //Console.WriteLine("multiplication:" + multiplication);
+            //Console.WriteLine("divide:" + divide);
+            //Console.WriteLine("difference:" + difference);
+            //Console.Read();
+
+            //string name, surname, department, lesson;
+            //int t1, t2, t3, average;
+            //name = "Mehmet";
+            //surname = "Kaymakcı";
+            //department = "Software Engineering";
+            //lesson = "Algorithm programming";
+            //t1 = 70;
+            //t2 = 80;
+            //t3 = 90;
+            //average = (t1 + t2 + t3) / 3;
+            //Console.WriteLine("***** Student Information System *****");
+            //Console.WriteLine();
+            //Console.WriteLine("Student name and surname:" + name + " " + surname);
+            //Console.WriteLine("Department:" + department);
+            //Console.WriteLine("Lesson:" + lesson);
+            //Console.WriteLine();
+            //Console.WriteLine("t1:" + t1);
+            //Console.WriteLine("t2:" + t2);
+            //Console.WriteLine("t3:" + t3);
+            //Console.WriteLine("average:" + average);
+            //Console.Read();
+
+            //double num;
+            //num = 24.9;
+            //Console.WriteLine(num);
+            //Console.Read();
+
+            //double n1, n2, avg;
+            //n1 = 30;
+            //n2 = 21;
+            //avg = (n1 + n2) / 2;
+            //Console.WriteLine(avg);
+            //Console.Read();
+
+            //string city, district;
+            //Console.Write("Please enter your city:");
+            //city = Console.ReadLine();
+            //Console.Write("Please enter your district:");
+            //district = Console.ReadLine();
+            //Console.WriteLine("The city and district you entered:" + city + "-" + district);
+            //Console.Read();
+
+            //int n;
+            //Console.Write("Enter the number:");
+            //n = Convert.ToInt16(Console.ReadLine());
+            //Console.Write(n);
+            //Console.Read();
+
+            //int n1, n2, total;
+            //Console.Write("n1:");
+            //n1 = Convert.ToInt16(Console.ReadLine());
+            //Console.Write("n2:");
+            //n2 = Convert.ToInt16(Console.ReadLine());
+            //total = n1 + n2;
+            //Console.Write(total);
+            //Console.Read();
+
+            //int shortEdge, longEdge, area, perimeter;
+            //Console.Write("Short Edge:");
+            //shortEdge = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Long Edge:");
+            //longEdge = Convert.ToInt32(Console.ReadLine());
+            //area = shortEdge * longEdge;
+            //perimeter = 2 * area;
+            //Console.WriteLine("area:" + area);
+            //Console.WriteLine("perimeter:" + perimeter);
+            //Console.ReadLine();
+
+            //int n1, n2, total, multiplication, divide, difference;
+            //Console.Write("n1:");
+            //n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("n2:");
+            //n2 = Convert.ToInt32(Console.ReadLine());
+            //total = n1 + n2;
+            //multiplication = n1 * n2;
+            //divide = n1 / n2;
+            //difference = n1 - n2;
+            //Console.WriteLine("total:" + total);
+            //Console.WriteLine("multiplication:" + multiplication);
+            //Console.WriteLine("divide:" + divide);
+            //Console.WriteLine("difference:" + difference);
+            //Console.ReadLine();
+
+            //double n1, n2, total;
+            //Console.Write("n1:");
+            //n1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("n2:");
+            //n2 = Convert.ToDouble(Console.ReadLine());
+            //total = n1 + n2;
+            //Console.Write("total:" + total);
+            //Console.ReadLine();
+
+            //char var;
+            //var = 'a';
+            //Console.Write(var);
+            //Console.Read();
+
+            //char var;
+            //var = '+';
+            //Console.Write(var);
+            //Console.Read();
+
+            //char selection;
+            //Console.Write("Please make your choice:");
+            //selection = Convert.ToChar(Console.ReadLine());
+            //Console.Write("Your choice:" + selection);
+            //Console.Read();
+
+            //byte n1, n2, total;
+            //n1 = 20;
+            //n2 = 30;
+            //total = Convert.ToByte(n1 + n2);
+            //Console.Write(total);
+            //Console.Read();
+
+            //byte n1, n2, multiplication;
+            //Console.Write("n1:");
+            //n1 = Convert.ToByte(Console.ReadLine());
+            //Console.Write("n2:");
+            //n2 = Convert.ToByte(Console.ReadLine());
+            //multiplication = Convert.ToByte(n1 * n2);
+            //Console.Write(multiplication);
+            //Console.Read();
+
+            //sbyte n1;
+            //n1 = 123;
+            //Console.Write(n1);
+            //Console.Read();
+
+            //sbyte n;
+            //Console.Write("n:");
+            //n = Convert.ToSByte(Console.ReadLine());
+            //Console.Write(n);
+            //Console.Read();
+
+            //sbyte n1, n2, total;
+            //Console.Write("n1:");
+            //n1 = Convert.ToSByte(Console.ReadLine());
+            //Console.Write("n2:");
+            //n2 = Convert.ToSByte(Console.ReadLine());
+            //total = Convert.ToSByte(n1 + n2);
+            //Console.Write(total);
+            //Console.Read();
+
+            //float number;
+            //number = 34.5f;
+            //Console.Write(number);
+            //Console.Read();
+
+            //float n1, n2, total;
+            //Console.Write("n1:");
+            //n1 = float.Parse(Console.ReadLine());
+            //Console.Write("n2:");
+            //n2 = float.Parse(Console.ReadLine());
+            //total = n1 + n2;
+            //Console.Write(total);
+            //Console.Read();
+
+            //Console.WriteLine(byte.MaxValue);
+            //Console.WriteLine(byte.MinValue);
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(float.MinValue);
+            //Console.WriteLine(sbyte.MaxValue);
+            //Console.WriteLine(sbyte.MinValue);
+            //Console.Read();
+
+            //short n1, n2, total;
+            //n1 = 650;
+            //n2 = 200;
+            //total = (short)(n1 + n2);
+            //Console.Write(total);
+            //Console.Read();
+
+            //short a, b, c;
+            //Console.Write("a:");
+            //a = short.Parse(Console.ReadLine());
+            //Console.Write("b:");
+            //b = short.Parse(Console.ReadLine());
+            //c = Convert.ToInt16(a + b);
+            //Console.Write(c);
+            //Console.Read();
+
+            //ushort num1, num2, total;
+            //num1 = 100;
+            //num2 = 200;
+            //total = (ushort)(num1 + num2);
+            //Console.Write(total);
+            //Console.Read();
+
+            //ushort shortEdge, longEdge, area, perimeter;
+            //Console.Write("Enter the short edge:");
+            //shortEdge = ushort.Parse(Console.ReadLine());
+            //Console.Write("Enter the long edge:");
+            //longEdge = ushort.Parse(Console.ReadLine());
+            //area = (ushort)(shortEdge * longEdge);
+            //perimeter = (ushort)(2 * area);
+            //Console.Write("area:" + area + " perimeter:" + perimeter);
+            //Console.Read();
+
+            //decimal num1, num2, difference;
+            //num1 = 20;
+            //num2 = 10;
+            //difference = num1 - num2;
+            //Console.Write(difference);
+            //Console.Read();
+
+            //decimal edge, area, perimeter;
+            //Console.Write("Enter the edge:");
+            //edge = Convert.ToDecimal(Console.ReadLine());
+            //area = edge * edge;
+            //perimeter = edge * 4;
+            //Console.Write("area:" + area + " perimeter" + perimeter);
+            //Console.Read();
+
+            //decimal num;
+            //num = 4.56m;
+            //Console.Write(num);
+            //Console.Read();
+
+            //bool situation;
+            //situation = true;
+            //Console.Write("Did the student pass the exam: ");
+            //Console.Write(situation);
+            //Console.Read();
+
+            //bool is_he_a_member;
+            //Console.Write("Is the user a member of the system: ");
+            //is_he_a_member = Convert.ToBoolean(Console.ReadLine());
+            //Console.Write("Membership status of the user to the system: " + is_he_a_member);
+            //Console.Read();
+        }
+    }
+}
