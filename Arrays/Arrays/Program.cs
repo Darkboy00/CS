c120cc92f5ec61b845d5f223e85bb6275c96120a
MehmetKaymakçı <61612352+Darkboy00@users.noreply.github.com>
2022-06-27 22:27:12 +0300

    Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] cities = { "adana", "bursa", "ankara", "istanbul" };
+            //Console.WriteLine(cities[0]);
+            //Console.WriteLine(cities[1]);
+            //Console.WriteLine(cities[2]);
+            //Console.WriteLine(cities[3]);
+            //Console.Read();10,20,30,40,50,60,70
+;
+            //int[] numbers = { 10, 20, 30, 40, 50, 60, 70 };
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //Console.Read();
+
+            //int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 20 == 0 & numbers[i] % 30 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+            //Console.Read();
+
+            //string[] persons = { "ali", "veli", "ahmet", "eylül", "mehmet" };
+            //for (int i = 0; i < persons.Length; i++)
+            //{
+            //    Console.WriteLine(persons[i]);
+            //}
+            //Console.Write(persons.Length);
+            //Console.Read();
+
+            //int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            //int total = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    total += numbers[i];
+            //}
+            //Console.WriteLine(total);
+            //Console.Read();
+
+            //int[] numbers = { 20, 14, 13, 35, 1 };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //Console.Read();
+
+            //string[] cities = { "Adana", "Ankara", "Zonguldak", "Malatya", "Istanbul", "Bursa" };
+            //Array.Sort(cities);
+            //Array.Reverse(cities);
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+            //Console.Read();
+
+            //string[] persons = { "Buse", "Ali", "Hakan", "Furkan", "Eylül" };
+            //int sequence;
+            //Array.Sort(persons);
+            //sequence = Array.IndexOf(persons, "Hakan");
+            //Console.Write(sequence);
+            //Console.Read();
+
+            //int[] numbers = { 76, 43, 12, 55, 34 };
+            //Console.WriteLine(numbers.Min());
+            //Console.Write(numbers.Max());
+            //Console.Read();
+
+            //string[] cities = { "ankara", "adana", "istanbul", "bursa" };
+            //foreach (string c in cities)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            //Console.Read();
+
+            //int[] numbers = { 23, 55, 33, 16, 89, 70 };
+            //int total = 0;
+            //foreach (int x in numbers)
+            //{
+            //    total = total + x;
+            //}
+            //Console.Write(total);
+            //Console.Read();
+
+            //int[] numbers = { 34, 22, 11, 67, 89, 50 };
+            //Array.Sort(numbers);
+            //foreach (int num in numbers)
+            //{
+            //    if(num % 2 == 0)
+            //    {
+            //        Console.WriteLine(num);
+            //    }
+            //}
+            //Console.Read();
+
+            string[] cities = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("City name: ");
+                cities[i] = Console.ReadLine();
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(cities[j]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello World");
+            //}
+            //Console.Read();
+
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.Read();
+
+            //for (int j = 0; j <= 20; j += 2)
+            //{
+            //    Console.WriteLine(j);
+            //}
+            //Console.Read();
+
+            //for (int k = 1; k <= 100; k+=1)
+            //{
+            //    Console.WriteLine(k + " Hello Istanbul");
+            //}
+            //Console.Read();
+
+            //int number = 24 % 9;
+            //Console.Write(number);
+            //Console.Read();
+
+            //int number;
+            //Console.Write("Enter the number:");
+            //number = Convert.ToInt16(Console.ReadLine());
+            //if(number % 2 == 0)
+            //{
+            //    Console.Write("number is even");
+            //}
+            //else
+            //{
+            //    Console.Write("number is odd");
+            //}
+            //Console.Read();
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.Read();
+
+            //int number;
+            //Console.Write("Enter the number: ");
+            //number = Convert.ToInt16(Console.ReadLine());
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.Read();
+
+            //int bacterium = 1;
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bacterium = bacterium * 2;
+            //    Console.WriteLine(bacterium);
+            //}
+            //Console.Read();
+
+            //int total = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    total = total + i;
+            //}
+            //Console.Write(total);
+            //Console.Read();
+
+            //int factorial = 1;
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    factorial = factorial * i;
+            //}
+            //Console.Write(factorial);
+            //Console.Read();
+
+            //int counter = 1;
+            //while (counter <= 10)
+            //{
+            //    Console.WriteLine("Hello World");
+            //    counter++;
+            //}
+            //Console.Read();
+
+            //int number = 1;
+            //while(number <= 20)
+            //{
+            //    Console.WriteLine(number);
+            //    number++;
+            //}
+            //Console.Read();
+
+            //int number = 1;
+            //int total = 0;
+            //while (number <= 10)
+            //{
+            //    total = total + number;
+            //    number++;
+            //}
+            //Console.Write(total);
+            //Console.Read();
+
+            //int number;
+            //int factorial = 1;
+            //Console.Write("Enter the number: ");
+            //number = Convert.ToInt16(Console.ReadLine());
+            //while (number >= 1)
+            //{
+            //    factorial = factorial * number;
+            //    number--;
+            //}
+            //Console.Write(factorial);
+            //Console.Read();
+
+            //int num = 12;
+            //while(num <= 10)
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+            //}
+            //Console.Read();
+
+            //int number = 12;
+            //do
+            //{
+            //    Console.WriteLine(number);
+            //    number++;
+            //}
+            //while (number <= 10);
+            //Console.Read();
+        }
+    }
+}
