3c1f0f871e9d955fd5c451dbd5e33a5733f81a7c
MehmetKaymakçı <61612352+Darkboy00@users.noreply.github.com>
2022-07-02 12:48:05 +0300

    DiceGame

---
+++ DiceGame/DiceGame/Dice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    class Dice
+    {
+        /// <summary>
+        /// maximum value variable that the dice can take
+        /// </summary>
+        int maximumDiceValue;
+
+        Random random = new Random();
+
+        /// <summary>
+        /// enter the maximum value the dice can take
+        /// </summary>
+        /// <param name="maximumDiceValue"></param>
+        public Dice(int maximumDiceValue)
+        {
+            this.maximumDiceValue = maximumDiceValue;
+        }
+        /// <summary>
+        /// roll a dice for a random number
+        /// </summary>
+        /// <returns></returns>
+        public int rollTheDice()
+        {
+            return random.Next(1, maximumDiceValue + 1);
+        }
+    }
+}
---
+++ DiceGame/DiceGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice(8);
+            Console.WriteLine("dice: " + dice.rollTheDice());
+            Console.Read();
+        }
+    }
+}
