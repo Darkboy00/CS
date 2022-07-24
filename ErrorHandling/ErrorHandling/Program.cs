bbdd1b4604a7d81b5f581db57a912e8ba86e8516
MehmetKaymakçı <61612352+Darkboy00@users.noreply.github.com>
2022-07-24 14:58:35 +0300

    ErrorHandling

---
+++ ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            try
+            {
+                int n1, n2, t;
+                Console.Write("n1:");
+                n1 = Convert.ToInt16(Console.ReadLine());
+                Console.Write("n2:");
+                n2 = Convert.ToInt16(Console.ReadLine());
+                t = n1 * n2;
+                Console.Write(t);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                Console.Write(ex);
+            }
+            
+            try
+            {
+                int n1, n2, t;
+                Console.Write("n1:");
+                n1 = Convert.ToInt16(Console.ReadLine());
+                Console.Write("n2:");
+                n2 = Convert.ToInt16(Console.ReadLine());
+                t = n1 * n2;
+                Console.Write(t);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter numeric value");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Value out of range");
+            }
+            */
+            try
+            {
+                int n1, n2, t;
+                Console.Write("n1:");
+                n1 = Convert.ToInt16(Console.ReadLine());
+                Console.Write("n2:");
+                n2 = Convert.ToInt16(Console.ReadLine());
+                t = n1 * n2;
+                Console.WriteLine(t);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                Console.Write("Hi");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ErrorHandling/ErrorHandling/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ErrorHandling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ErrorHandling")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b420c0d0-8ac2-4bca-b193-c297e680bd50")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
